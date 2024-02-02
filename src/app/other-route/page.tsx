37711a7538a686778e901747f7a152da8cee1b6d
Paweł Sowa <pawel@the-sowa.com>
2024-02-02 23:20:30 +0100

    feat: add pokemon lists with server action

---
+++ src/app/other-route/page.tsx
@@ -0,0 +1,14 @@
+import { PokemonCard } from "../components/PokemonCard";
+import { getPokemon } from "./api";
+
+export default async function OtherRoute() {
+  const pokemon = await getPokemon({ limit: 9, offset: 9 });
+
+  return (
+    <main className="max-w-[1200px] m-auto p-10 grid md:grid-cols-3 gap-10">
+      {pokemon.results.map((pokemon) => (
+        <PokemonCard key={pokemon.name} {...pokemon} />
+      ))}
+    </main>
+  );
+}
